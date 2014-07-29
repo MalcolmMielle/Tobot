b96a97aab3ef79009b3b75dfc0bed6147bcdaa71
MalcolmMielle <malcolm.mielle@gmail.com>
2014-07-30 00:52:36 +0800

    Corrected the number of tick

--- Tobotdriver/include/motor_drivers/SerialPortControl_stream.hpp
+++ Tobotdriver/include/motor_drivers/SerialPortControl_stream.hpp
@@ -56,7 +56,7 @@ class SerialPortControl{
 		_pnode.param<std::string>("NodeleftWheel", _LWHEEL, "1");
 		_pnode.param<std::string>("NodeleftRheel", _RWHEEL, "2");
 		_pnode.param<std::string>("NodeLifter", _LIFTER, "3");
-		_pnode.param<double>("TICKNUM", _nTick, 200000);
+		_pnode.param<double>("TICKNUM", _nTick, 600000);
 		_pnode.param<double>("TICKNUMLIFT", _nTickLift, 500000);
 		//_LWHEEL="1";
 		//_RWHEEL="1";
